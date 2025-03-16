7a4f882a2e751b58eb1aa731b50e6fec90bfca0e
Adrian <adriansanduta@gmail.com>
2025-03-15 19:44:53 -0500

    todolist1.1

--- Todo List Starter/app.js
+++ Todo List Starter/app.js
@@ -7,23 +7,33 @@
 
 const list = document.querySelector('.list');  
 
-const todoList = [
+let todoList = [
     {
+        id: 1,
         task: 'Finish Frontend Simplified'
     },
     {
+        id: 2,
         task: 'Finish Interview Questions'
     },
     {
+        id: 3,
         task: 'Land $100k Job'
     },
-]
+];
 
+function deleteTodo(id) {
+    todoList = 
+  todoList.filter((todo) => {return todo.id !== id});
+}
+
+function renderTodos() {
 list.innerHTML = todoList.map((element) => 
     `<li>
         ${element.task}
-        <button class="todo__delete">
+        <button class="todo__delete" onclick="deleteTodo(${element.id})">
             x
         </button>
     </li>`
 ).join('');
+}
